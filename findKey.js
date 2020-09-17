3717ece91f5800d549c8b2496393b12f18573a05
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-09-17 22:42:35 +0000

    added findKey function with callback

---
+++ findKey.js
@@ -0,0 +1,10 @@
+const findKey = function (object, callback) {
+  for(let key in object) {
+    console.log('key:', key); 
+    console.log('value', object[key]); 
+    if(callback(object[key])) {
+      return key; 
+    }
+  }
+  return undefined; 
+};
--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,5 +1,4 @@
 const findKeyByValue = function (object, value) {
-  let firstKey = '';
   for(let key in object) {
     if(object[key] === value) {
       return key; 
