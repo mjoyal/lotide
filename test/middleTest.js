69657cfe18270809046380f327ef99ae1eff082d
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-09-21 20:49:23 +0000

    added a middleTest.js file to check if middleis working

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,1 +1,6 @@
-const 
+const middle = require('../middle'); 
+const assertArraysEqual = require('../assertArraysEqual'); 
+
+assertArraysEqual(middle(["a","b", "c", "d", "e"]), ['c']); 
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7 ,8]), [4,5]); 
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7 ,8]), [8, 9]); 
