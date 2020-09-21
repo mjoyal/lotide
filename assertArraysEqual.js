0e42a480567febcba90ca725490658259fe253c5
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-09-21 20:28:45 +0000

    refractered assertArraysEqual to import the eqArray function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,13 @@
-const assertArraysEqual = function (array1, array2) {
-  if(array1.length !== array2.length) {
+const eqArrays = require('./eqArrays'); 
+
+const assertArraysEqual = function (eqArrays) {
+  if(!eqArrays) {
     console.log("🛑🛑🛑 Assertion Failed:  Arrays are not identical.");
   } else {
-    for(let i = 0; i < array1.length; i++) {
-      if(array1[i] !== array2[i]) {
-        console.log("🛑🛑🛑 Assertion Failed:  Arrays are not identical.");
-      }
-    }
+    console.log("✅✅✅ Assertion Passed: Arrays are identical.");
   }
-  console.log("✅✅✅ Assertion Passed: Arrays are identical.");
-};
+}; 
+
+assertArraysEqual(eqArrays);
 
 module.exports = assertArraysEqual; 
--- eqArrays.js
+++ eqArrays.js
@@ -11,9 +11,5 @@ const eqArrays = function (array1, array2) {
   return true; 
 };
 
+module.exports = eqArrays; 
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));// => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));// => false
